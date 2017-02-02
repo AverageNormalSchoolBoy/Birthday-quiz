b3baa794c8d3b5cf40e098d70799dbea90b8b53c
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-02 14:40:58 -0500

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -38,10 +38,10 @@ todaydate = datetime.today().day
 
 month1 = month_name[todaymonth]
  
-name = input("Hello, what is your name?")
-month = input("Hi {0}, what was the name of the month you were born in?" .format(name))
-year = input("And what year were you born in, {0}?" .format(name))
-day = input("And the day?")
+name = input("Hello, what is your name? ")
+month = input("Hi {0}, what was the name of the month you were born in? " .format(name))
+year = input("And what year were you born in, {0}? " .format(name))
+day = input("And the day? ")
 month = month.lower()
 month1 = month1.lower()
 
